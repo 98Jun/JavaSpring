69d4d3120ad0389c7e650d76fdaf84a1acd6f85d
Administrator <Administrator@User -2022NWARS>
2022-08-29 17:02:56 +0900

    .

--- jex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ jex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -11,27 +11,56 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@ContextConfiguration(classes= {org.zerock.config.RootConfig.class})
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@ContextConfiguration(classes= {org.zerock.config.RootConfig.class})
 @Log4j
 public class BoardMapperTests {
-	
-	@Setter(onMethod_ = @Autowired)
-	private BoardMapper mapper;
-	
-	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
-	}
-	
-	 @Test
-	   public void testInsert() {
-		   BoardVO board = new BoardVO();
-		   board.setTitle("엄준식 화이팅");
-		   board.setContent("내용은 살아있따");
-		   board.setWriter("new sunsik");
-		   mapper.insert(board);
 
-		   log.info(board);
-	   }
-}
+   @Setter(onMethod_=@Autowired)
+   private BoardMapper mapper;
+   
+   @Test
+   public void testGetList() {
+   mapper.getList().forEach(board -> log.info(board));
+   }
+   
+   
+   @Test
+   public void testInsert() {
+      BoardVO board = new BoardVO();
+      board.setTitle("고추장 닭날개 조림");
+      board.setContent("근데 이제 바질을 곁들인");
+      board.setWriter("newbie");
+      
+      mapper.insert(board);   
+      
+      log.info(board);
+   }
+   
+   @Test
+   public void testRead() {
+      
+      BoardVO board = mapper.read(5L);
+      //5번 게시물 읽기
+      
+      log.info(board);
+   }
+      @Test
+      public void testDelete() {
+         log.info("delete count: " +mapper.delete(3L));
+         }
+      
+      @Test
+      public void testUpdate() {
+         BoardVO board = new BoardVO();
+         
+         board.setBno(5L);
+         board.setTitle("간장닭날개조림");
+         board.setContent("짭조롬..");
+         board.setWriter("user00");
+         
+         int count=mapper.update(board);
+         log.info("UPDATE COUNT:"+count);
+      }
+         
+   }
