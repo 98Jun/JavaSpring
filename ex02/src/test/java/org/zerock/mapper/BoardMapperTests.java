d29f0b05295f4ea7e1a49f066a81c26f96bc27f2
Administrator <Administrator@User -2022NWARS>
2022-08-29 17:31:44 +0900

    220829

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -13,6 +13,7 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 //@ContextConfiguration(classes= {org.zerock.config.RootConfig.class})
+
 @Log4j
 public class BoardMapperTests {
 
