df4008efd6a66531154eb87f7205c5974de39179
Administrator <Administrator@User -2022NWARS>
2022-08-29 17:47:20 +0900

    220829

--- jex02/src/test/java/org/zerock/service/BoardServiceTests.java
+++ jex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -13,7 +13,7 @@ import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration("file:src/main/webapp/WEB_INF/spring/root-context.xml")
-@ContextConfiguration(classes = {org.zerock.config.RootConfig.class)
+@ContextConfiguration(classes = {org.zerock.config.RootConfig.class})
 @Log4j
 public class BoardServiceTests {
 	
